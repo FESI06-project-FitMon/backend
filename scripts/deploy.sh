e1abd0dee8efb6fc43f92e84301c21a51efd0f04
JWbase <jungwoo1002@gmail.com>
2024-12-30 17:46:53 +0900

    chore: deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fla java | grep jar | awk '{print $1}')
+CURRENT_PID=$(lsof -ti tcp:8080)
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -18,6 +18,12 @@ cd $REPOSITORY
 
 echo "> Git Pull"
 git pull origin dev
+
+echo "> 로컬 변경 사항을 버리고 원격 dev 브랜치와 동기화"
+git reset --hard origin/dev
+
+echo "> 필요 없는 파일 제거"
+git clean -fd
 
 echo "> 프로젝트 Build 시작"
 ./gradlew clean build -x test
