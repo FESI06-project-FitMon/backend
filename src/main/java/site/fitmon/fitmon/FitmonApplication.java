5287ced88a647ad37f6f01fe106407f80ffc01bf
JWbase <jungwoo1002@gmail.com>
2024-12-30 11:12:24 +0900

    init

---
+++ src/main/java/site/fitmon/fitmon/FitmonApplication.java
@@ -0,0 +1,13 @@
+package site.fitmon.fitmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/fitmon/fitmon/FitmonApplicationTests.java
@@ -0,0 +1,13 @@
+package site.fitmon.fitmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
