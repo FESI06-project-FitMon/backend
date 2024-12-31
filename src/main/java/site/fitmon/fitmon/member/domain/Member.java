1218961cdb001bf4aa898ccfb263f94452db5d4a
JWbase <jungwoo1002@gmail.com>
2024-12-31 11:18:23 +0900

    feat: 유저 도메인 구현

---
+++ src/main/java/site/fitmon/fitmon/member/domain/Member.java
@@ -0,0 +1,35 @@
+package site.fitmon.fitmon.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.fitmon.fitmon.common.domain.BaseEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String profileImageUrl;
+}
