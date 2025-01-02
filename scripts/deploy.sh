b2dc44a3bb86570dd66f783ad045da4c24c31482
JWbase <jungwoo1002@gmail.com>
2025-01-02 17:07:25 +0900

    refactor: 배포스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,14 +12,7 @@ done
 echo "Starting new application..."
 echo "Database username: $DB_USERNAME"
 sudo -E java -jar \
--Dspring.datasource.url="jdbc:mysql://localhost:3306/fitmon" \
--Dspring.datasource.username="$DB_USERNAME" \
--Dspring.datasource.password="$DB_PASSWORD" \
--Dspring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQLDialect \
--Dspring.cloud.aws.credentials.accessKey="$AWS_S3_ACCESS_KEY" \
--Dspring.cloud.aws.credentials.secretKey="$AWS_S3_SECRET_ACCESS_KEY" \
--Dspring.cloud.aws.s3.bucket="$AWS_S3_BUCKET_NAME" \
--Dspring.cloud.aws.region.static="$AWS_S3_SECRET_REGION" \
+-Dspring.config.location=file:/home/ubuntu/app/application.yml \
 cicd.jar > output.log 2>&1 &
 
 echo "Waiting for application to start..."
