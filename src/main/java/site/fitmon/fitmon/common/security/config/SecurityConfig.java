93d858fc38a7063b8353eeb6fc0ea50295a41f53
JWbase <jungwoo1002@gmail.com>
2025-01-02 16:20:46 +0900

    feat: 시큐리티 설정 추가

--- src/main/java/site/fitmon/fitmon/common/security/config/SecurityConfig.java
+++ src/main/java/site/fitmon/fitmon/common/security/config/SecurityConfig.java
@@ -1,20 +1,34 @@
 package site.fitmon.fitmon.common.security.config;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import site.fitmon.fitmon.common.security.jwt.JwtAuthenticationFilter;
+import site.fitmon.fitmon.common.security.jwt.JwtTokenProvider;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -28,13 +42,16 @@ public class SecurityConfig {
                         requestMatchers(
                             "/",
                             "/v3/api-docs/**",
-                            "/swagger-ui/**"
+                            "/swagger-ui/**",
+                            "/api/v1/signup",
+                            "/api/v1/login"
                         ).permitAll()
                         .anyRequest()
                         .authenticated())
             .sessionManagement(
                 sessionManagement ->
                     sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
