c9de22be24239cf5fd2e5a5d79fd1c74d00ef1d7
JWbase <jungwoo1002@gmail.com>
2025-01-03 17:01:03 +0900

    feat: s3 예외처리 추가

--- src/main/java/site/fitmon/fitmon/common/exception/GlobalExceptionHandler.java
+++ src/main/java/site/fitmon/fitmon/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package site.fitmon.fitmon.common.exception;
 
+import static site.fitmon.fitmon.common.exception.ErrorCode.*;
 import static site.fitmon.fitmon.common.exception.ErrorCode.INVALID_INPUT_VALUE;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -31,5 +34,11 @@ public class GlobalExceptionHandler {
 
         ErrorResponse errorResponse = new ErrorResponse(INVALID_INPUT_VALUE, errors);
         return new ResponseEntity<>(errorResponse, errorResponse.getStatus());
+    }
+
+    @ExceptionHandler(S3Exception.class)
+    public ResponseEntity<ErrorResponse> handleS3Exception(S3Exception e) {
+        ErrorResponse response = new ErrorResponse(IMAGE_UPLOAD_FAILED);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
