51e38fd82efddb7d02f52b4ad70e2084f24658ac
JWbase <jungwoo1002@gmail.com>
2025-01-08 15:20:50 +0900

    refactor: cors 설정 변경

--- src/main/java/site/fitmon/common/security/config/SecurityConfig.java
+++ src/main/java/site/fitmon/common/security/config/SecurityConfig.java
@@ -74,10 +74,11 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(
             List.of(
                 "http://localhost:3000",
-                "https://api.fitmon.site"));
+                "https://api.fitmon.site",
+                "http://localhost:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setExposedHeaders(List.of("Authorization"));
+//        configuration.setExposedHeaders(List.of("Authorization"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
