2cbd5d8f2b3b806df1f22f5ac7b00968b708ae5f
JWbase <jungwoo1002@gmail.com>
2025-02-11 18:02:55 +0900

    refactor: 비밀번호 유효성 검사 변경

--- src/main/java/site/fitmon/auth/dto/request/SignupRequest.java
+++ src/main/java/site/fitmon/auth/dto/request/SignupRequest.java
@@ -2,6 +2,7 @@ package site.fitmon.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,8 +20,11 @@ public class SignupRequest {
     @Size(min = 2, max = 10, message = "닉네임은 2 ~ 10자 사이로 입력해주세요.")
     private String nickName;
 
-    @Schema(description = "비밀번호", example = "password1234")
-    @Size(message = "비밀번호는 8자 이상이어야 합니다.", min = 8)
+    @Schema(description = "비밀번호", example = "Password@123")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        message = "비밀번호는 대문자, 소문자, 숫자, 특수문자를 포함하여 8자 이상이어야 합니다."
+    )
     private String password;
 
     public Member toEntity(String encodedPassword) {
